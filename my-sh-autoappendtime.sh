4639d7efeba20ffb8425013dda47d67e7cbdaacd
LiZhenhai-MBP14 <lizhenhai@LiZhenhaideMacBookPro14.local>
2024-07-09 21:12:08 +0800

    update

--- my-sh-autoappendtime.sh
+++ my-sh-autoappendtime.sh
@@ -29,7 +29,7 @@ LAST_UPDATE_DATE=$(git log -1 --format=%ad --date=short -- "$FILE")
 
 # 检查文件中是否已经有创建日期
 if ! grep -q "^> 本文创建日期:" "$FILE"; then
-  echo -e "\n---\n> 本文创建日期: $CREATION_DATE" >> "$FILE"
+  echo -e "\n\n---\n> 本文创建日期: $CREATION_DATE\n" >> "$FILE"
 fi
 
 # 删除已有的最后更新日期信息
@@ -39,6 +39,6 @@ sed -i '' -e '/^> 最后更新日期:/d' "$FILE"
 #sed -i '' -e :a -e '/^\n*$/{$d;N;};/\n$/ba' "$FILE"
 
 # 追加新的最后更新日期信息到文件末尾
-printf "> 最后更新日期: $LAST_UPDATE_DATE" >> "$FILE"
+printf "\n> 最后更新日期: $LAST_UPDATE_DATE" >> "$FILE"
 
 
